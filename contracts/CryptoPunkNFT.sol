e7820ada811aa3838c5d56c9e860f296339ebadd
ntourne <ntourne@beerealit.com>
2021-12-29 19:01:51 -0300

    Install hardhat. Create new NFT contract + tests

---
+++ contracts/CryptoPunkNFT.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CryptoPunkNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public maxSupply;
+
+    string public baseURI;
+
+    event NFTMinted(address sender, uint256 tokenId);
+
+    constructor(uint256 _maxSupply, string memory __baseURI)
+        ERC721("Crypto Punk", "PUNK")
+    {
+        // It increments tokenIds counter so first NFT minted starts with 1 instead of 0
+        _tokenIdCounter.increment();
+
+        baseURI = __baseURI;
+        maxSupply = _maxSupply;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function mint(address to) public {
+        // Get current tokenId and increment one
+        // It allows that first token id minted is 1
+        uint256 tokenId = _tokenIdCounter.current();
+
+        require(
+            tokenId <= maxSupply,
+            "Total supply of tokens have been minted"
+        );
+
+        _safeMint(to, tokenId);
+
+        _tokenIdCounter.increment();
+
+        emit NFTMinted(to, tokenId);
+    }
+
+    function totalMinted() public view returns (uint256) {
+        return _tokenIdCounter.current() - 1;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/cryptopunknft.test.js
@@ -0,0 +1,101 @@
+const { expect } = require('chai');
+const hardhat = require('hardhat');
+
+const { ethers } = hardhat;
+
+const BASE_URI = 'https://domain.com/';
+
+async function deploy(maxSupply = 10000, baseURI = BASE_URI) {
+    const [owner, alice, bob, mark] = await ethers.getSigners();
+
+    let factory = await ethers.getContractFactory("CryptoPunkNFT", owner);
+    const contract = await factory.deploy(maxSupply, baseURI);
+
+    const minterFactory = factory.connect(alice)
+    const minterContract = minterFactory.attach(contract.address)
+
+    return {
+        contract,
+        minterContract,
+        owner,
+        alice,
+        bob,
+        mark
+    }
+}
+
+
+describe('CryptoPunkNFT', () => {
+
+    it('Should deploy', async () => {
+        const [owner] = await ethers.getSigners();
+
+        const CryptoPunkNFTFactory = await ethers.getContractFactory('CryptoPunkNFT', owner);
+        const cryptoPunkNFT = await CryptoPunkNFTFactory.deploy(5000, BASE_URI);
+        const contract = await cryptoPunkNFT.deployed();
+
+        await expect(await contract.owner()).to.equals(owner.address, 'Owner is not correct');
+        await expect((await contract.maxSupply()).toNumber()).to.equals(5000, 'Incorrect number of maxSupply of tokens');
+    });
+
+    it('Should mint a token', async () => {
+        const { contract, alice } = await deploy();
+
+        await contract.mint(alice.address);
+        await expect((await contract.totalMinted()).toNumber()).to.equals(1, 'Incorrect number of tokens minted');
+    });
+
+
+    it('Should fail to mint a token if max supply is reached', async () => {
+        const { minterContract, alice } = await deploy(3);
+
+        await minterContract.mint(alice.address);
+        await minterContract.mint(alice.address);
+        await minterContract.mint(alice.address);
+
+        await expect((await minterContract.totalMinted()).toNumber()).to.equals(3, 'Incorrect number of tokens minted');
+
+        await expect(minterContract.mint(alice.address))
+            .to.be.revertedWith('Total supply of tokens have been minted');
+
+        await expect((await minterContract.totalMinted()).toNumber()).to.equals(3, 'Incorrect number of tokens minted');
+    });
+
+    it('Should return baseUri', async () => {
+        const { minterContract, alice } = await deploy();
+        await minterContract.mint(alice.address);
+        await expect(await minterContract.tokenURI(1)).to.equals(BASE_URI + 1, 'Incorrect token URI');
+    });
+
+    it('Should change baseURI', async () => {
+        const { contract } = await deploy();
+        await expect(await contract.baseURI()).to.equals(BASE_URI, 'Incorrect base URI before change');
+
+        const newBaseURI = 'https://newbaseuri.com/';
+        await contract.setBaseURI(newBaseURI);
+        await expect(await contract.baseURI()).to.equals(newBaseURI, 'Incorrect base URI after change');
+    });
+
+    it('Should not allow change baseURI if not owner', async () => {
+        const { minterContract } = await deploy();
+
+        await expect(await minterContract.baseURI()).to.equals(BASE_URI, 'Incorrect base URI before change');
+        await expect(minterContract.setBaseURI('https://fakeurl.com/')).to.be.revertedWith('Ownable: caller is not the owner');
+        await expect(await minterContract.baseURI()).to.equals(BASE_URI, 'Incorrect base URI after failed change');
+    });
+
+    it('Should burn a token', async () => {
+        const { minterContract, alice } = await deploy();
+
+        await minterContract.mint(alice.address);
+
+        await minterContract._burn(1);
+    })
+
+    it("Should support interface", async function () {
+
+        const { contract } = await deploy();
+        await expect(await contract.supportsInterface(0x01ffc9a7))
+            .to.be.equals(true);
+    })
+})
